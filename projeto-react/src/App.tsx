bac1c9b3306c58430e305c98bf60f340ba607b1d
victorcosmedev <victorcosme.profissional@gmail.com>
2024-08-20 19:51:32 -0300

    Props sendo passada como parâmetro

--- projeto-react/src/App.tsx
+++ projeto-react/src/App.tsx
@@ -1,7 +1,19 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
 function App(){
+  const titulo:string = "Minha Página";
+  // const numero:number = 0;
+  // const listaDeNomes:string[] = ["João", "Maria", "Pedro"];
+  // const aluno:{nome:string, idade:number, altura:number} = {
+  //   nome: "João",
+  //   idade: 20,
+  //   altura: 1.80
+  // }
+
   return(
     <div>
       <h1>Componente App</h1>
+      <Cabecalho tituloProps={titulo}/>
     </div>
   );
 }
--- projeto-react/src/components/Cabecalho/Cabecalho.tsx
+++ projeto-react/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,11 @@
-export default function Cabecalho(){
+function Cabecalho(props:{tituloProps: string}){
+
+    document.title = props.tituloProps;
     return(
         <header className="cabecalho">
-            
+            <h1>{props.tituloProps}</h1>
         </header>
     );
 }
+
+export default Cabecalho;
