152d7546b5499f9ce5e9d1e4e7ff273c6fd74c3b
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-08-20 19:50:53 -0300

    props sendo passada como variavel

--- aula-05/src/App.tsx
+++ aula-05/src/App.tsx
@@ -1,12 +1,20 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
 
 
 
 export default function App() {
  
-
+  const titulo:string = "Minha Pagina";
+  const lista:string[] = ["o","a","v"];
+  const aluno:{nome:string,idade:number,altura:number} = {
+    nome: "JOAO",
+    idade: 20,
+    altura: 1.92
+  }
   return (
     <>
       <h1>teste</h1>
+      <Cabecalho tituloProps={titulo}/>
     </>
   )
 }
